ee35ea54228cfd4a475f14239dc1013a5662532c
SWASTIC-7 <swastic_k@ee.iitr.ac.in>
2025-11-02 22:45:05 +0530

    clippy

--- src/assembler/pass1asm.rs
+++ src/assembler/pass1asm.rs
@@ -162,24 +162,23 @@ pub fn pass1asm(buffer: &str) -> (Vec<LabeledParsedLines>, u32, u32, Vec<SymbolT
                             //TODO
                         }
                         "ORG" => {
-                            
                             let operand: Option<String> = lines.operand1.clone();
 
-                                let address: u32 = if let Some(expr) = &operand {
-                                    match evaluate_operand_expression(expr, &symbol_table) {
-                                        Ok(val) => val,
-                                        Err(e) => {
-                                            log_info(&format!(
-                                                "Failed to evaluate ORG expression '{}': {}",
-                                                expr, e
-                                            ));
-                                            locctr
-                                        }
+                            let address: u32 = if let Some(expr) = &operand {
+                                match evaluate_operand_expression(expr, &symbol_table) {
+                                    Ok(val) => val,
+                                    Err(e) => {
+                                        log_info(&format!(
+                                            "Failed to evaluate ORG expression '{}': {}",
+                                            expr, e
+                                        ));
+                                        locctr
                                     }
-                                } else {
-                                    locctr
-                                };
-                                locctr = address;
+                                }
+                            } else {
+                                locctr
+                            };
+                            locctr = address;
                         }
                         "WORD" => {
                             if let Some(label) = lines.label.clone() {
